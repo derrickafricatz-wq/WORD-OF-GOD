81e3cdad4f2e5e7c09945ca847a1f9954851cdff
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-04-14 19:32:54 +0300

    Update bible-data.js

--- bible-data.js
+++ bible-data.js
@@ -1,25 +1,8 @@
 const bibleBooks = {
-  book1: {
-    title: "📖"– How to See God",
+  genesis: {
+    title: "Genesis",
     pages: [
-      [
-        "GOD'S FIRST LANGUAGE IS NOT ENGLISH",
-
-        "The Lord is always speaking, but He is not talkative.",
-        "He is more determined to speak to us than we are to hear Him.",
-
-        "God is always speaking, but He is not human.",
-        "His first language is not English."
-      ],
-
-      [
-        "RELATIONSHIP WITH GOD",
-
-        "Leading by God means allowing Him to guide your thoughts.",
-        "It is a lifestyle built on relationship, not rules.",
-
-        "Start with relationship, not religion."
-      ]
+      ["In the beginning..."]
     ]
   }
 };
